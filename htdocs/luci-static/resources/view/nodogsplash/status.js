b842f2c98d31d023ae32e6fe6dcff97cb96b9e4e
Douglas Orend <douglas.orend@comcast.net>
2024-05-29 14:44:24 -0500

    v2.0 - Updated status.js

--- htdocs/luci-static/resources/view/nodogsplash/status.js
+++ htdocs/luci-static/resources/view/nodogsplash/status.js
@@ -1,9 +1,9 @@
 'use strict';
-'require dom';
 'require fs';
-'require poll';
 'require ui';
 'require view';
+
+var sep;
 
 return view.extend({
 	load: function() {
@@ -19,20 +19,52 @@ return view.extend({
 		});
 	},
 
-	render: function(logdata) {
-		var loglines = logdata.trim().split(/\n/);
+	render_table: function(output, start) {
+		var lines = output.trim().split(/\n/);
+		var arr, out, table, idx, data
+		
+        var table = E('table', { 'class': 'table', 'id': 'status' });
+		if (lines.length == 1) {
+			table.appendChild(E('tr', { 'class': 'tr' }, [
+				E('td', { 'class': 'td left' }, _("ndsctl: nodogsplash probably not started (Error: Connection refused)"))
+			]));
+		} else {
+        	for (var i = start; i < lines.length; i++) {
+        		if (lines[i].substring(0,3) == "===") { 
+        			sep = i + 1
+        			break 
+        		}
+        		data = lines[i].split(":")
+        		idx = data[0];
+        		data.shift();
+        		data = data.join(":").trim();
+        		if (data.substring(0,7) == "http://" || data.substring(0,8) == "https://") {
+        			data = E('a', { 'href': data, 'target': '_blank' }, data);
+        		}
+        		table.appendChild(E('tr', { 'class': 'tr' }, [
+	                E('td', { 'class': 'td left', 'width': '33%' }, _(idx)),
+            		E('td', { 'class': 'td left' }, data)
+        		]));
+	        }
+	    }
+	    return table;
+	},
 
-		return E([], [
-			E('h2', {}, [_('NoDogSplash Status')]),
-			E('div', { 'id': 'content_syslog' }, [
-				E('textarea', {
-					'id': 'syslog',
-					'readonly': 'readonly',
-					'wrap': 'off',
-					'rows': loglines.length + 1
-				}, [ loglines.join('\n') ])
-			])
-		]);
+	render: function(output) {
+        return E('div', { 'class': 'cbi-map', 'id': 'map' }, [
+            E('div', { 'class': 'left' }, [
+                E('h3', _('NoDogSplash Status')),
+                this.render_table(output, 3)
+            ]),
+            E('div', { 'class': 'left' }, [
+                E('h3', _('NoDogSplash Clients')),
+                this.render_table(output, sep)
+            ]),
+            E('div', { 'class': 'left' }, [
+                E('h3', _('Type of Clients')),
+                this.render_table(output, sep)
+            ]),
+        ]);
 	},
 
 	handleSave: null,
